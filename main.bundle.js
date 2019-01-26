0b63ef03b6cabf8370b15fe87d9fec31a36a0084
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-25 18:06:53 -0700

    Remove comment

--- main.bundle.js
+++ main.bundle.js
@@ -44,8 +44,22 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	// This file is in the entry point in your webpack config.
 	"use strict";
+
+	//
+	var locbutton = document.getElementById("location-btn");
+
+	function getWeather() {
+	  var location = document.getElementById("location-field").value;
+	  var request = new XMLHttpRequest();
+	  request.open('GET', "https://safe-reaches-47529.herokuapp.com/api/v1/forecast?location=" + location);
+	  request.onload = function () {
+	    var jsonData = JSON.parse(request.responseText)["data"];
+	    console.log(jsonData);
+	  };
+	  request.send();
+	}
+	locbutton.addEventListener("click", getWeather);
 
 /***/ })
 /******/ ]);
