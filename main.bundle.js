f1d922df2caacfe62496a21f6a709511aa410588
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-27 23:49:57 -0700

    Updates bundles.

--- main.bundle.js
+++ main.bundle.js
@@ -109,14 +109,14 @@
 	  var today = responseData.daily_weather[0];
 	  var location = document.getElementById("location-field").value;
 	  var date = new Date();
-	  $("#forecast-sum").append("<div>\n        <h8 >" + responseData.current_weather.summary + "</h8></br>\n        <h8 id = \"temp\">" + responseData.current_weather.temperature + "&deg</h8>\n        <p>High:" + today.temp_high + "&deg Low:" + today.temp_low + "&deg</p>\n      </div>\n      <div>\n      <h3>" + location + "</h3>\n      </div>\n      <div>\n        <p><a font-weight=\"normal\" id=\"favorite-button\" href=\"\">Favorite</a></p>\n        <p><a font-weight=\"normal\" id=\"delete-button\" href=\"\"> Delete Favorite</a></p>\n        <p><a font-weight=\"normal\" id=\"change-button\" href=\"\" onclick=\"clearForm()\"> Change Location</a></p>\n      </div>");
+	  $("#forecast-sum").append("<div>\n        <h8 >" + responseData.current_weather.summary + "</h8></br>\n        <h8 id = \"temp\">" + responseData.current_weather.temperature + "&deg</h8>\n        <p>High:" + today.temp_high + "&deg Low:" + today.temp_low + "&deg</p>\n      </div>\n      <div>\n      <h3>" + location + "</h3>\n      </div>\n      <div>\n        <p><a font-weight=\"normal\" class=\"links\" id=\"favorite-button\" href=\"\">Favorite</a></p>\n        <p><a font-weight=\"normal\" class=\"links\" id=\"delete-button\" href=\"\"> Delete Favorite</a></p>\n        <p><a font-weight=\"normal\" class=\"links\" id=\"change-button\" href=\"\" onclick=\"clearForm()\"> Change Location</a></p>\n      </div>");
 	}
 
 	function createWeatherDetails(detailData) {
 	  var currentForecast = detailData.current_weather;
 	  var today = detailData.daily_weather[0];
 	  var tonight = detailData.hourly_weather[6];
-	  $("#forecast-details").append("<div>\n        <h2>Details</h2>\n        <div><i class=\"wi wi-snow\"></i></div>\n\n        <h3 id=\"test-id\">" + currentForecast.summary + "</h3></br>\n        <p>Today: " + today.summary + "</p>\n        <p>Tonight: " + tonight.summary + "</p>\n         </div>\n         <div>\n         <p>Feels Like: " + currentForecast.feels_like + "&deg</p>\n         <p>Humidity: " + currentForecast.humidity + "%</p>\n         <p>Visibility: " + currentForecast.visibility + " miles</p>\n         <p>Uv Index: " + currentForecast.uv_index + "</p>\n         </div>");
+	  $("#forecast-details").append("<div>\n        <p>Details</p>\n        <div><i class=\"wi wi-snow\"></i></div>\n        <p id=\"test-id\">" + currentForecast.summary + "</p>\n        <p class=\"smaller-text\">Today: " + today.summary + "</p>\n        <p class=\"smaller-text\">Tonight: " + tonight.summary + "</p>\n         </div>\n         <div>\n         <p>Feels Like: " + currentForecast.feels_like + "&deg</p>\n         <p>Humidity: " + currentForecast.humidity + "%</p>\n         <p>Visibility: " + currentForecast.visibility + " miles</p>\n         <p>Uv Index: " + currentForecast.uv_index + "</p>\n         </div>");
 	}
 
 	function createHourlySummary(hourlyData) {
@@ -127,7 +127,7 @@
 	  var hourFive = hourlyData.hourly_weather[4];
 	  var hourSix = hourlyData.hourly_weather[5];
 
-	  $("#summary-hours").append("\n        <div> <p>" + hourOne.time + "</p><p> <i class=\"wi wi-stars\"></i></p> <p>" + hourOne.temperature + "&deg</p></div>\n        <div><p>" + hourTwo.time + "</p> <p> <i class=\"wi wi-stars\"></i></p><p>" + hourTwo.temperature + "&deg</p> </div>\n        <div><p>" + hourThree.time + "</p><p> <i class=\"wi wi-stars\"></i></p> <p>" + hourThree.temperature + "&deg</p>  </div>\n        <div><p>" + hourFour.time + "</p> <p> <i class=\"wi wi-stars\"></i></p><p>" + hourFour.temperature + "&deg</p>  </div>\n        <div><p>" + hourFive.time + "</p><p> <i class=\"wi wi-stars\"></i></p> <p>" + hourFive.temperature + "&deg</p> </div>\n        <div><p>" + hourSix.time + "</p><p> <i class=\"wi wi-stars\"></i></p> <p>" + hourSix.temperature + "&deg</p>  </div>\n        ");
+	  $("#summary-hours").append("\n        <div> <p>" + hourOne.time + "</p><p> <i id=\"hour-icon\" class=\"wi wi-moon-new\"></i></p> <p>" + hourOne.temperature + "&deg</p></div>\n        <div><p>" + hourTwo.time + "</p> <p> <i id=\"hour-icon\" class=\"wi wi-moon-new\"></i></p><p>" + hourTwo.temperature + "&deg</p> </div>\n        <div><p>" + hourThree.time + "</p><p> <i id=\"hour-icon\" class=\"wi wi-moon-new\"></i></p> <p>" + hourThree.temperature + "&deg</p>  </div>\n        <div><p>" + hourFour.time + "</p> <p> <i id=\"hour-icon\" class=\"wi wi-moon-new\"></i></p><p>" + hourFour.temperature + "&deg</p>  </div>\n        <div><p>" + hourFive.time + "</p><p> <i id=\"hour-icon\" class=\"wi wi-moon-new\"></i></p> <p>" + hourFive.temperature + "&deg</p> </div>\n        <div><p>" + hourSix.time + "</p><p> <i id=\"hour-icon\" class=\"wi wi-moon-new\"></i></p> <p>" + hourSix.temperature + "&deg</p>  </div>\n        ");
 	};
 
 	function createWeeklyOverview(dailyData) {
